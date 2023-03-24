238b263502bbd77677a03787e404789fd2b22e24
John Ellie Go <ellie.go12@gmail.com>
2023-03-24 18:41:18 +0800

    Initial Commit

---
+++ burgers-ios/burgers_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  burgers_iosApp.swift
+//  burgers-ios
+//
+//  Created by John Ellie Go on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct burgers_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
